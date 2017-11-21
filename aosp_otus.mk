fc3ec7a7113fae087f31f3e562b5defad71b7f16
Elanza-48 <rishikundu6@gmail.com>
2017-11-21 14:57:30 +0530

    added more changes

--- aosp_otus.mk
+++ aosp_otus.mk
@@ -1,11 +1,14 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Use Magisk
  +DEFAULT_ROOT_METHOD := magisk
- +
+
+# Build Type
+EXTENDED_BUILD_TYPE=UNOFFICIAL
+
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_otus
